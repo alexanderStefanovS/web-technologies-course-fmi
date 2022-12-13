d1bbc8166c06f27b16161f2e5b1dde7157b26a88
alexanderStefanovS <a.stefanov_grad@abv.bg>
2022-12-13 19:07:03 +0200

    js 2

---
+++ kn-2022/exercise-10-js-2/index.js
@@ -0,0 +1,59 @@
+
+console.log(this);
+
+function fn() {
+
+    function fn2() {
+        console.log(this);
+    }
+
+    fn2();
+}
+
+function getAge() {
+    return this.age;
+}
+
+// const person = {
+//     age: 24,
+//     name: 'Sasho',
+//     getName: function () {
+//         console.log(this);
+
+//         // function fn() {
+//         //     console.log(this);
+//         // }
+
+//         // fn();
+
+//         return this.name;
+//     }
+// }
+
+// class Person {
+
+//     constructor(name, age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     getName() {
+//         return this.name;
+//     }
+
+// }
+
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+// getAge.call(person);
+
+(function () {
+
+    console.log('IIFE');
+
+})();
+
+
